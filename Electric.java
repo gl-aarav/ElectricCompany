f7b7b88a3ee4ea5abb1e1271d48994f6306374c4
gl-aarav <gl.aarav@gmail.com>
2024-10-30 19:06:39 -0700

    Create Electric.java

---
+++ Electric.java
@@ -0,0 +1,43 @@
+/* 
+ * Aarav Goyl
+ * 10/30/24
+ * Electric.java
+ * 
+ * This program prompts the user to enter which plan a user wants to take a 
+ * residential bill, commercial bill, or an industrial bill.
+ * 
+ * Working on: if-else statements for conditional logic.
+ * 
+ * Testing: 
+ * Residential, hours = 1000, fee = $107.00
+ * Commercial, hours = 1000, fee = $203.00
+ * Industrial, peakhours = 100, off-peakhours = 150, fee = $260.00
+ * Residential, hours = 1, fee = $12.095
+ * Commercial, hours = 1001, fee = $120.083
+ * Industrial, peakhours = 1000, off-peakhours = 1000, fee = $260.00;
+ * 
+ * Pseudocode:
+ * 
+ * main: Initializes the program by creating an instance of the Electric class.
+ * 
+ * getData: 
+ * - 
+ * calculateCost:
+ * -
+ * 
+ */
+ 
+import java.util.Scanner; // Import the Scanner class for user input
+public class Electric 
+{
+	public Electric()
+	{
+		
+	}
+	
+	public static void main (String[] args){
+		
+	}
+	
+	
+}
